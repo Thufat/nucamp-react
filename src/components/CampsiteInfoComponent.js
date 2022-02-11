74fc3ff08765fb152b8a56b389d90e88c493c60f
Thufat <92640620+Thufat@users.noreply.github.com>
2022-02-11 11:25:42 -0800

    second Server commit

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -39,7 +39,7 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+    this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
@@ -134,7 +134,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -154,7 +154,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
             </div>
           );
         })}
-        <CommentForm  campsiteId={campsiteId} addComment={addComment}/>
+        <CommentForm  campsiteId={campsiteId} postComment={postComment}/>
       </div>
     );
   }
@@ -199,7 +199,7 @@ function CampsiteInfo(props) {
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
           <RenderComments comments={props.comments} 
-          addComment={props.addComment}
+          postComment={props.postComment}
           campsiteId={props.campsite.id}
           />
         </div>
