70e96f6696c801f1d16977261da1417cf90df044
Thufat <92640620+Thufat@users.noreply.github.com>
2022-02-12 17:03:17 -0800

    last assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseURL';
+import { partnersLoading } from '../redux/ActionCreators';
+import { Loading } from "./LoadingComponent";
 
 
 function RenderPartner({partner}){
@@ -20,15 +23,36 @@ function RenderPartner({partner}){
     }
 }
 
-function About(props) {
+function PartnerList (props) {
 
     const partners = props.partners.map(partner => {
+
         return (
             <Media tag="li" key={partner.id}>
-                <RenderPartner partner={partner} />
+                <RenderPartner src={ baseUrl + partner.image} partner={partner} />
             </Media>
         );
     });
+
+    if (partnersLoading()) {
+        return (
+            <Loading/>
+        )
+    } else {
+        <div className="col">
+            <h4>{props.errMess}</h4>
+        </div>
+    }
+
+    <div className="col mt-4">
+        <Media list>
+            {partners}
+        </Media>
+    </div>
+     
+}
+
+function About(props) {
 
     return (
         <div className="container">
@@ -83,9 +107,7 @@ function About(props) {
                     <h3>Community Partners</h3>
                 </div>
                 <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
+                    <PartnerList partners={props.partners} />
                 </div>
             </div>
         </div>
